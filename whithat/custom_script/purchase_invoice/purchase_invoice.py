1a807a523997677c0315a6bd5fefdd42cc7c09aa
Remat Noyda <rematmtpl@gmail.com>
2024-05-21 15:52:25 +0530

    resolve sli not create

--- whithat/custom_script/purchase_invoice/purchase_invoice.py
+++ whithat/custom_script/purchase_invoice/purchase_invoice.py
@@ -3,8 +3,8 @@ from erpnext.accounts.doctype.purchase_invoice.purchase_invoice import PurchaseI
 from erpnext.buying.utils import update_last_purchase_rate
 
 class CustomPurchaseInvoice(PurchaseInvoice):
+
     def on_submit(self):
-        print('\n\n\n\n>>>> on submit')
+        super(CustomPurchaseInvoice, self).on_submit()
         if not frappe.db.get_single_value("Buying Settings", "disable_last_purchase_rate"):
-            print('>> update last purchase rate')
             update_last_purchase_rate(self, is_submit=1)
