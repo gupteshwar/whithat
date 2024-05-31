21e69b8e5f97937bce724155fa46f73f845215e5
Remat Noyda <rematmtpl@gmail.com>
2024-05-31 18:54:25 +0530

    add credit notes in subscription

--- whithat/public/js/sales_invoice.js
+++ whithat/public/js/sales_invoice.js
@@ -1,7 +1,8 @@
 frappe.ui.form.on('Sales Invoice', {
     refresh: function(frm){
-        if (frm.doc.custom_subscription != ''){
-            console.log('>> From Subscription')
+        if (!frm.doc.custom_subscription){
+            console.log('>> Not Subscription')
+        }else{
             if (frm.doc.is_return !== 1){
                 frm.set_df_property('items','cannot_delete_rows',true)
             }
