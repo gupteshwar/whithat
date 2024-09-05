b95d56b3599176cdf8c31aaa4efcb7677eb7eb15
Midocean <remat.n@midocean.tech>
2024-09-05 17:49:42 +0530

    new changes

--- whithat/custom_script/sales_invoice/sales_invoice.py
+++ whithat/custom_script/sales_invoice/sales_invoice.py
@@ -13,8 +13,11 @@ class CustomSalesInvoice(SalesInvoice):
             if customer.sales_team:
                 for i in customer.sales_team:
                     allocated_amount = flt((self.amount_eligible_for_commission * i.allocated_percentage) / 100.0) if i.allocated_percentage else 0.00
-                    commission_rate = i.commission_rate
-                    incentives = flt((allocated_amount * float(commission_rate.strip('%'))) / 100.0) if i.commission_rate and allocated_amount else 0.00
+                    if '%' in i.commission_rate and i.commission_rate:
+                        commission_rate = float(i.commission_rate.strip('%'))
+                    else:
+                        commission_rate = float(i.commission_rate) if i.commission_rate else 0.00
+                    incentives = flt((allocated_amount * commission_rate) / 100.0) if commission_rate and allocated_amount else 0.00
                     self.append("sales_team", {
                         "sales_person": i.sales_person,
                         "allocated_percentage": i.allocated_percentage,
